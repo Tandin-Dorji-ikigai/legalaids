7d29ef934b658b75eae1b297c2df89c402e3a41f
Thogmey <dorjixt@gmail.com>
2024-11-27 17:40:23 +0600

    changed the document icons in DetailsPopup from trash and add to Eye

--- src/components/DetailsPopup.jsx
+++ src/components/DetailsPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useState, forwardRef, useEffect } from "react";
-import { X, Trash2, Plus, Minus } from "lucide-react";
+import { X, Trash2, Plus,Eye, Minus } from "lucide-react";
 import "./DetailsPopup.css";
 import { useGetCaseQuery } from "../slices/caseApiSlice";
 import { useUpdateCaseMutation } from "../slices/caseApiSlice";
@@ -280,13 +280,10 @@ const DetailsPopup = forwardRef(({ caseId, onClose }, ref) => {
                           <span className="document-filename">asdf</span>
                         </div>
                         <div className="document-actions">
-                          <button className="icon-button delete">
-                            <Trash2 size={18} />
-                          </button>
-                          {/* Only include add button if needed */}
                           <button className="icon-button add">
-                            <Plus size={18} />
+                            <Eye size={18} />
                           </button>
+                          
                         </div>
                       </div>
                     </div>
