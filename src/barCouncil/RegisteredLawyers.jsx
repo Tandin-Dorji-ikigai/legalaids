2763175a4a62894bb21951c1715d908284c2bfe6
Tandin Penjor <sanchezpenjor@gmail.com>
2024-11-30 09:29:05 +0600

    changes

--- src/barCouncil/RegisteredLawyers.jsx
+++ src/barCouncil/RegisteredLawyers.jsx
@@ -1,84 +1,85 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SideNav from "./DashboardNav";
-import { useGetAllLawyerQuery } from "../slices/lawyerSlice";
-import { useEnableLawyerMutation } from "../slices/lawyerSlice";
-import { useDisableLawyerMutation } from "../slices/lawyerSlice";
+import { useGetAllLawyerQuery, useEnableLawyerMutation, useDisableLawyerMutation } from "../slices/lawyerSlice";
 import Swal from "sweetalert2";
+import Loader from "../components/Loader";
 
 function EmployeeManagement() {
-  const { data: lawyers, error } = useGetAllLawyerQuery();
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const casesPerPage = 10;
+
+  const { data: lawyers, error, isLoading } = useGetAllLawyerQuery();
   const [enableLawyer] = useEnableLawyerMutation();
   const [disableLawyer] = useDisableLawyerMutation();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredLawyers, setFilteredLawyers] = useState([]);
 
   useEffect(() => {
-    if (error) {
-      console.log(error);
+    if (lawyers) {
+      handleSearch(searchQuery);
     }
-  }, [error, lawyers]);
+  }, [lawyers, searchQuery]);
 
-  const handleEnableLawyer = (id) => {
-    Swal.fire({
-      title: "",
+  const handleEnableLawyer = async (id) => {
+    const confirmed = await Swal.fire({
       text: "Are you sure you want to enable the user?",
       icon: "question",
       showCancelButton: true,
       confirmButtonColor: "#1E306D",
       cancelButtonColor: "#d33",
       confirmButtonText: "Confirm",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          await enableLawyer(id).unwrap();
-          Swal.fire({
-            title: 'Success!',
-            text: 'User successfully enabled.',
-            icon: 'success',
-            confirmButtonText: 'OK'
-          });
-        } catch (err) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Failed to enable the user. Please try again.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-        }
-      }
     });
-  }
+    if (confirmed.isConfirmed) {
+      try {
+        await enableLawyer(id).unwrap();
+        Swal.fire("Success!", "User successfully enabled.", "success");
+        window.location.reload()
+      } catch {
+        Swal.fire("Error!", "Failed to enable the user. Please try again.", "error");
+      }
+    }
+  };
 
-  const handleDisableLawyer = (id) => {
-    Swal.fire({
-      title: "",
+  const handleDisableLawyer = async (id) => {
+    const confirmed = await Swal.fire({
       text: "Are you sure you want to disable the user?",
       icon: "question",
       showCancelButton: true,
       confirmButtonColor: "#1E306D",
       cancelButtonColor: "#d33",
       confirmButtonText: "Confirm",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          await disableLawyer(id).unwrap();
-          Swal.fire({
-            title: 'Success!',
-            text: 'User successfully disabled.',
-            icon: 'success',
-            confirmButtonText: 'OK'
-          });
-        } catch (err) {
-          Swal.fire({
-            title: 'Error!',
-            text: 'Failed to disable the user. Please try again.',
-            icon: 'error',
-            confirmButtonText: 'OK'
-          });
-        }
-      }
     });
-  }
+    if (confirmed.isConfirmed) {
+      try {
+        await disableLawyer(id).unwrap();
+        Swal.fire("Success!", "User successfully disabled.", "success");
+        window.location.reload()
+      } catch {
+        Swal.fire("Error!", "Failed to disable the user. Please try again.", "error");
+      }
+    }
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    if (lawyers) {
+      const filtered = lawyers.filter((lawyer) =>
+        Object.values(lawyer).some((value) =>
+          String(value).toLowerCase().includes(query.toLowerCase())
+        )
+      );
+      setFilteredLawyers(filtered);
+    }
+  };
+
+  const totalPages = Math.ceil(lawyers.length / casesPerPage);
+  const indexOfLastCase = currentPage * casesPerPage;
+  const indexOfFirstCase = indexOfLastCase - casesPerPage;
+  const currentCases = lawyers.slice(indexOfFirstCase, indexOfLastCase);
+
   return (
-    <div className="dashboard-container ">
+    <div className="dashboard-container">
       <SideNav />
       <div className="dashboard-content">
         <div className="dashboard-header">Registered Lawyers</div>
@@ -86,7 +87,14 @@ function EmployeeManagement() {
           <div className="users-header">Lawyers</div>
           <div className="search-bar">
             <div className="search-bar-container">
-              <input type="text" placeholder="Search" />
+              <input
+                type="text"
+                placeholder="Search"
+                value={searchQuery}
+                onChange={(e) => handleSearch(e.target.value)}
+                aria-label="Search lawyers"
+                style={{ color: "black" }}
+              />
               <div className="search-bar-icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -98,11 +106,15 @@ function EmployeeManagement() {
                   <path d="M765-144 526-383q-30 22-65.79 34.5-35.79 12.5-76.18 12.5Q284-336 214-406t-70-170q0-100 70-170t170-70q100 0 170 70t70 170.03q0 40.39-12.5 76.18Q599-464 577-434l239 239-51 51ZM384-408q70 0 119-49t49-119q0-70-49-119t-119-49q-70 0-119 49t-49 119q0 70 49 119t119 49Z" />
                 </svg>
               </div>
+
             </div>
           </div>
-          <div className="details-container lawyer-details-container">
-            <div className="lawyer-details lawyer-details-sm">
-              <h3>Lawyer Details</h3>
+          {isLoading ? (
+            <Loader />
+          ) : error ? (
+            <div>Error loading lawyers. Please try again later.</div>
+          ) : filteredLawyers.length > 0 ? (
+            <div className="details-container lawyer-details-container">
               <table>
                 <thead>
                   <tr>
@@ -114,24 +126,51 @@ function EmployeeManagement() {
                   </tr>
                 </thead>
                 <tbody>
-                  {lawyers &&
-                    lawyers.map((lawyer) => {
-                      return (
-                        <tr key={lawyer.cid}>
-                          <td>{lawyer.cid}</td>
-                          <td>{lawyer.userName}</td>
-                          <td>{lawyer.email}</td>
-                          <td>{lawyer.contactNo}</td>
-                          <td>{lawyer.enabled === true ?
-                            <button onClick={() => handleDisableLawyer(lawyer.id)} className="disable_user">Disable</button>
-                            :
-                            <button onClick={() => handleEnableLawyer(lawyer.id)} className="enable_user">Enable</button>} </td>
-                        </tr>
-                      );
-                    })}
+                  {filteredLawyers.map((lawyer) => (
+                    <tr key={lawyer.cid}>
+                      <td>{lawyer.cid}</td>
+                      <td>{lawyer.userName}</td>
+                      <td>{lawyer.email}</td>
+                      <td>{lawyer.contactNo}</td>
+                      <td>
+                        {lawyer.enabled ? (
+                          <button
+                            onClick={() => handleDisableLawyer(lawyer.id)}
+                            className="disable_user"
+                          >
+                            Disable
+                          </button>
+                        ) : (
+                          <button
+                            onClick={() => handleEnableLawyer(lawyer.id)}
+                            className="enable_user"
+                          >
+                            Enable
+                          </button>
+                        )}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
+
               </table>
             </div>
+          ) : (
+            []
+          )}
+
+          <div className="pagination">
+            <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+              Previous
+            </button>
+            {Array.from({ length: totalPages }, (_, index) => (
+              <button key={index + 1} onClick={() => setCurrentPage(index + 1)} className={currentPage === index + 1 ? 'active' : ''}>
+                {index + 1}
+              </button>
+            ))}
+            <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>
+              Next
+            </button>
           </div>
         </div>
       </div>
